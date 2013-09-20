26097e5517fb51d485acb4399fd1d2ac100c9381
kevinced <w450991479@gmail.com>
2013-09-20 21:01:14 +0800

    更新

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
 #include <stm32f10x.h>
 int main()
-{}
+{	while(1)
+	{
+		KeyScan();		
+		Measure_Wave();
+//		USART1_printf( USART1, "\r\n this is a demo \r\n" );
+//		MyPrintf("%d,fasfda, %s\n", cs1, "hello");
+	}
+}
